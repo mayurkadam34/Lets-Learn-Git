10d7d1053a8a73ac80bc3a8ff6a600b4df03cca5
Mayur Kadam <mayurkadam04@gmail.com>
2026-06-03 16:38:37 +0530

    updated

--- Multiply.java
+++ Multiply.java
@@ -3,7 +3,7 @@ public class Multiply
     public static void main(String[] args)
     {
        int a=5;
-       int b=6;
+       int b=6; 
 
        system.out.println(a*b);
     }
---
+++ diff.java
@@ -0,0 +1,10 @@
+public class Diff
+{
+    public static void main(string[] args)
+    {
+            int a=5;
+            int b=6;       
+
+       system.out.println(a-b);
+    }
+}
